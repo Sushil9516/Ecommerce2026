b1e35ac829c23642e4e4f187bd59893eeebaa697
Sushil Kumar Soni <sonisushil9516@gmail.com>
2025-12-29 23:05:01 +0530

    Making Folder structure and set the Outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/Layout";
+import AuthLogin from "./pages/auth/Login";
+import AuthRegister from "./pages/auth/Register";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <div className="flex flex-col overflow-hidden bg-white">
+        {/* Common Components */}
+        <h1>Header Component</h1>
+        <Routes>
+          <Route path="/*" element={<h1>Home Component</h1>} />
+          <Route path="/auth" element={<AuthLayout />}>
+            {" "}
+            {/* Nested(Parent) Routes for AuthLayout */}
+            <Route path="login" element={<AuthLogin />} />
+            <Route path="register" element={<AuthRegister />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/admin-view/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AdminLayout = () => {
+  return (
+    <div className="flex w-full min-h-screen">
+      {/* Admin SideBar */}
+      <div className="flex- flex-1 flex-col">
+        {/* Admin Header */}
+        <main className="flex flex-1 bg-muted/40 p-4 md:p-6">
+          <Outlet /> {/* used to render child routes */}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
