35b32bed8dec81e739d97602e6f210b4912d86be
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-01-04 23:45:47 +0530

    4-1-2026

--- client/src/components/admin-view/Layout.jsx
+++ client/src/components/admin-view/Layout.jsx
@@ -8,9 +8,7 @@ const AdminLayout = () => {
       <div className="flex- flex-1 flex-col">
         {/* Admin Header */}
         <main className="flex flex-1 bg-muted/40 p-4 md:p-6">
-        
           <Outlet /> {/* used to render child routes */}
-          
         </main>
       </div>
     </div>
